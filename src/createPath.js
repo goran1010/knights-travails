2025dde891f9772e7361b756d23974fabe0189e1
Goran Jovic <goran1010jovic@gmail.com>
2023-11-10 11:38:07 +0100

    Remove some logs

--- src/createPath.js
+++ src/createPath.js
@@ -1,5 +1,4 @@
 export default function createPath(currentPosition, allMoves) {
-  console.log(allMoves);
   if (!currentPosition.parentPosition) return allMoves;
   allMoves.push(currentPosition.parentPosition.coor);
   return createPath(currentPosition.parentPosition, allMoves);
